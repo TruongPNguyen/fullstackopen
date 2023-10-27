98892835ee1b46441ce53e9e8a174c7f7a84fa38
Truong Nguyen <reachtruong@gmail.com>
2023-10-26 17:30:23 -0700

    finished exercise 3.12

---
+++ part3/phonebook-backend/mongo.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+if (process.argv.length < 3) {
+    console.log("give passsword as argument")
+    process.exit()
+}
+
+const password = process.argv[2]
+const url = 
+    `mongodb+srv://truong:${password}@cluster0.bt0sbn0.mongodb.net/personApp?retryWrites=true&w=majority`
+
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    name : String,
+    number : String, 
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length == 3) {
+    Person
+        .find({})
+        .then(persons => {
+            console.log("Phonebook: ")
+            persons.forEach(person => {
+                console.log(person.name, person.number)
+            })
+            mongoose.connection.close()
+        })
+}
+if (process.argv.length > 4) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+    const person = new Person({
+        name : name,
+        number : number
+    })
+    person.save().then(result => {
+        console.log(`Added ${name}, number: ${number} to the phonebook`)
+        mongoose.connection.close()
+    })
+}

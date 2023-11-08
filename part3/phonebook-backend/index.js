1f5442b3110ff9145c80b674497604721d6c3644
Truong Nguyen <reachtruong@gmail.com>
2023-11-07 18:00:16 -0800

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -78,10 +78,10 @@ app.post("/api/persons", (request, response, next) => {
 app.put("/api/persons/:id", (request, response, next) => {
     const body = request.body;
     const id = request.params.id;
-    const person = new Person({
+    const person = {
         name : body.name,
         number : body.number
-    })
+    }
     Person.findByIdAndUpdate(id, person, { new : true })
         .then(updatedPerson => {
             response.json(updatedPerson)

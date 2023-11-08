2d5726b7d4201076ce416debf627804852767c12
Truong Nguyen <reachtruong@gmail.com>
2023-11-07 18:43:25 -0800

    uibuild

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -82,7 +82,8 @@ app.put("/api/persons/:id", (request, response, next) => {
         name : body.name,
         number : body.number
     }
-    Person.findByIdAndUpdate(id, person, { new : true })
+    Person.findByIdAndUpdate(id, person, 
+        { new : true, runValidators: true, context: "query" })
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
@@ -99,6 +100,8 @@ const errorHandler = (error, request, response, next) => {
     console.error(error.message)
     if (error.name === "CastError") {
         return response.status(400).send({ error : "malformatted id"})
+    } else if (error.name === "ValidationError") {
+        return response.status(400).json({ error : error.message })
     }
     next(error)
 }
